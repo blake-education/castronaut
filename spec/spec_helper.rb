7cfba88ed1447d4f498f0b9caa3c54890fdadbfd
Damien Le Berrigaud <damien.lb@gmail.com>
2011-04-28 13:51:56 +1000

    Cleaned spec migrations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,25 +9,25 @@ Castronaut.config = Castronaut::Configuration.load(File.join File.dirname(__FILE
 
 class CreateUsers < ActiveRecord::Migration
   Castronaut::Adapters::RestfulAuthentication::User.connection.create_table "users", :force => true do |t|
-    t.column :login,                     :string, :limit => 40
-    t.column :name,                      :string, :limit => 100, :default => '', :null => true
-    t.column :email,                     :string, :limit => 100
-    t.column :crypted_password,          :string, :limit => 40
-    t.column :salt,                      :string, :limit => 40
-    t.column :created_at,                :datetime
-    t.column :updated_at,                :datetime
-    t.column :remember_token,            :string, :limit => 40
-    t.column :remember_token_expires_at, :datetime
-    t.string :first_name, :last_name
+    t.string   :login,                    :limit => 40
+    t.string   :name,                     :limit => 100, :default => '', :null => true
+    t.string   :email,                    :limit => 100
+    t.string   :crypted_password,         :limit => 40
+    t.string   :salt,                     :limit => 40
+    t.datetime :created_at
+    t.datetime :updated_at
+    t.string   :remember_token,           :limit => 40
+    t.datetime :remember_token_expires_at
+    t.string   :first_name, :last_name
   end
   Castronaut::Adapters::RestfulAuthentication::User.connection.add_index :users, :login, :unique => true
 end
 
 class CreateUsers < ActiveRecord::Migration
   Castronaut::Adapters::Development::User.connection.create_table "users", :force => true do |t|
-    t.column :login,                     :string, :limit => 40
-    t.column :name,                      :string, :limit => 100, :default => '', :null => true
-    t.column :password,                  :string
+    t.string :login,    :limit => 40
+    t.string :name,     :limit => 100, :default => '', :null => true
+    t.string :password
   end
   Castronaut::Adapters::Development::User.connection.add_index :users, :login, :unique => true
 end
