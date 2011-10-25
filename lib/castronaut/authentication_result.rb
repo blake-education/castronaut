c7d2db03853515ac0ce9eabf8a4a52d75324346b
Damien Le Berrigaud <damien@itsdamo.com>
2011-10-25 16:22:10 +1100

    Updated AuthenticationResult to be able to have extra attributes
    Made the implementation backwards compatible by testing for the type. It's not so elegant but it allows minimum code changes, thus less bugs... hopefully.

--- lib/castronaut/authentication_result.rb
+++ lib/castronaut/authentication_result.rb
@@ -2,11 +2,13 @@ module Castronaut
 
   class AuthenticationResult
     
-    attr_reader :username, :error_message
+    attr_reader :username, :error_message, :extra_attributes
         
-    def initialize(username, error_message=nil)
+    def initialize(username, error_or_extra_attributes=nil)
       @username = username
-      @error_message = error_message
+      @error_message    = error_or_extra_attributes if error_or_extra_attributes.is_a? String
+      @extra_attributes = error_or_extra_attributes if error_or_extra_attributes.is_a? Hash
+
       Castronaut.logger.info("#{self.class} - #{@error_message} for #{@username}") if @error_message && @username
     end
     
--- spec/castronaut/authentication_result_spec.rb
+++ spec/castronaut/authentication_result_spec.rb
@@ -1,20 +1,29 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + "/../spec_helper"
 
 describe Castronaut::AuthenticationResult do
-  
+
   it "exposes the given username at :username" do
-    Castronaut::AuthenticationResult.new('billy').username.should == 'billy'
+    Castronaut::AuthenticationResult.new("billy").username.should == "billy"
   end
-  
+
   it "exposes the given message at :error_message" do
-    Castronaut::AuthenticationResult.new(anything, 'my error').error_message.should == 'my error'
+    Castronaut::AuthenticationResult.new(anything, "my error").error_message.should == "my error"
   end
-  
+
+  it "exposes the given extra attributes at :extra_attributes" do
+    Castronaut::AuthenticationResult.new(anything, {:hello => "world!"}).extra_attributes[:hello].should == "world!"
+  end
+
   it "is valid if there is no error message" do
     Castronaut::AuthenticationResult.new(anything, nil).should be_valid
   end
 
   it "is invalid if there is an error message" do
-    Castronaut::AuthenticationResult.new(anything, 'bad').should be_invalid
+    Castronaut::AuthenticationResult.new(anything, "bad").should be_invalid
   end
+
+  it "is valid if extra attributes are given" do
+    Castronaut::AuthenticationResult.new(anything, {:foo => "bar"}).should be_valid
+  end
+
 end
